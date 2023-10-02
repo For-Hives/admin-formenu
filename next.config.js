004ea223b3ec376873689fb3be1d89c0a5195fc0
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-02 18:14:10 +0200

    try to tfix

--- next.config.js
+++ next.config.js
@@ -31,18 +31,6 @@ const nextConfig = {
 			},
 		],
 	},
-	webpack: config => {
-		let modularizeImports = null
-		config.module.rules.some(rule =>
-			rule.oneOf?.some(oneOf => {
-				modularizeImports = oneOf?.use?.options?.nextConfig?.modularizeImports
-				return modularizeImports
-			})
-		)
-		if (modularizeImports?.['@headlessui/react'])
-			delete modularizeImports['@headlessui/react']
-		return config
-	},
 }
 
 module.exports = nextConfig
