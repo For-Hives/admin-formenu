220ea526d2ecb6b500cfaccdab8562a1f95e2b08
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-10 21:08:15 +0100

    🔧 Update font imports and metadata in layout.js
    This commit updates the font imports in `layout.js` to use the `next/font/google` package. It adds the Lato and Nunito fonts with their respective weights, subsets, variables, and styles. The commit also removes the old font links from the metadata object.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,32 @@ import '@/styles/globals.css'
 import WrapSessionProvider from '@/providers/WrapSessionProvider'
 import { StoreProvider } from '@/providers/StoreProvider'
 import WrapNextUiProvider from '@/providers/WrapNextUiProvider'
+import { Lato, Nunito } from 'next/font/google'
+
+const lato = Lato({
+	weight: ['100', '300', '400', '700', '900'],
+	subsets: ['latin'],
+	variable: '--font-lato',
+	style: ['normal', 'italic'],
+})
+
+const nunito = Nunito({
+	weight: [
+		'100',
+		'200',
+		'300',
+		'400',
+		'500',
+		'600',
+		'700',
+		'800',
+		'900',
+		'1000',
+	],
+	subsets: ['latin'],
+	variable: '--font-nunito',
+	style: ['normal', 'italic'],
+})
 
 export const metadata = {
 	title: 'Administration - ForMenu',
@@ -14,21 +40,6 @@ export const metadata = {
 			'en-US': 'https://formenu.net/admin',
 		},
 	},
-	links: [
-		{
-			rel: 'preconnect',
-			href: 'https://fonts.googleapis.com',
-		},
-		{
-			rel: 'preconnect',
-			href: 'https://fonts.gstatic.com',
-			crossOrigin: true,
-		},
-		{
-			rel: 'stylesheet',
-			href: 'https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
-		},
-	],
 }
 
 export default function RootLayout({ children }) {
