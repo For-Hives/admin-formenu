bb9d0c748544b8fd47457b5f2778ee6e6f38977d
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-10 21:30:45 +0100

    🔧 Update font weights in layout.js
    The commit updates the font weights in the `layout.js` file. The previous code listed each weight individually, but now they are condensed into a single array. This change improves readability and reduces redundancy.

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,18 +12,7 @@ const lato = Lato({
 })
 
 const nunito = Nunito({
-	weight: [
-		'100',
-		'200',
-		'300',
-		'400',
-		'500',
-		'600',
-		'700',
-		'800',
-		'900',
-		'1000',
-	],
+	weight: ['200', '300', '400', '500', '600', '700', '800', '900', '1000'],
 	subsets: ['latin'],
 	variable: '--font-nunito',
 	style: ['normal', 'italic'],
