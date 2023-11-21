afb4da30836b57571b1a80d6cd2c299ec116de2c
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-21 06:57:48 +0100

    🔧 Refactor ToggleMenuComponent and use a separate ToggleComponent
    This commit refactors the ToggleMenuComponent by extracting the toggle functionality into a separate ToggleComponent. The new component takes in an id, activated state, and onUpdate function as props. The updateMenu function is now async and handles toggling the menu state using the toggleMenuState function.

--- src/components/Toggle/ToggleMenu.component.js
+++ src/components/Toggle/ToggleMenu.component.js
@@ -1,31 +1,14 @@
 'use client'
 import React from 'react'
 import { useSession } from 'next-auth/react'
-import { cn, Switch } from '@nextui-org/react'
+import ToggleComponent from '@/components/Toggle/Toggle.component'
 
 export default function ToggleMenuComponent({ id, activated }) {
-	const [isSelected, setIsSelected] = React.useState(activated ?? true)
 	const { data: session } = useSession()
-	return (
-		<Switch
-			onChange={e => toggleMenuState(e.target.id, e.target.checked, session)}
-			isSelected={isSelected}
-			onValueChange={setIsSelected}
-			size="sm"
-			color="primary"
-			classNames={{
-				wrapper: cn(
-					'w-[95px] relative inline-flex cursor-pointer items-center p-2 h-[30px] rounded'
-				),
-				thumb: cn(
-					'rounded h-[20px] w-[20px] bg-white',
-					'group-data-[selected=true]:ml-[59px]'
-				),
-			}}
-			startContent={<p>&nbsp;&nbsp;&nbsp;activé</p>}
-			endContent={<p>désactivé&nbsp;</p>}
-		/>
-	)
+	const updateMenu = async (id, checked) => {
+		await toggleMenuState(id, checked, session)
+	}
+	return <ToggleComponent id={id} activated={activated} onUpdate={updateMenu} />
 }
 
 async function toggleMenuState(id, activated, session) {
