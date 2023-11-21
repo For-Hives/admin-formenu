59dbf0f27772581e29da9f4cf500f2e8fdc48a2f
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-21 07:01:45 +0100

    🔧 Refactor ToggleDishComponent to use ToggleComponent
    This commit refactors the ToggleDishComponent to use the newly created ToggleComponent. The code now imports and uses the ToggleComponent instead of manually rendering the toggle switch. Additionally, the function name `toggleMenuState` has been changed to `toggleDishState` for clarity and consistency.

--- src/components/Toggle/ToggleDish.component.js
+++ src/components/Toggle/ToggleDish.component.js
@@ -1,31 +1,17 @@
 'use client'
 import React from 'react'
 import { useSession } from 'next-auth/react'
+import ToggleComponent from '@/components/Toggle/Toggle.component'
 
 export default function ToggleDishComponent({ id, activated }) {
 	const { data: session } = useSession()
-	return (
-		<label className="relative inline-flex cursor-pointer items-center">
-			<input
-				name="activated"
-				type="checkbox"
-				id={id}
-				defaultChecked={activated}
-				onChange={e => toggleMenuState(e.target.id, e.target.checked, session)}
-				className="peer sr-only"
-			/>
-			<div
-				className="peer h-[28px] w-[100px] rounded bg-gray-200 after:absolute after:left-[4px]
-										after:top-[4px] after:h-[20px] after:w-[20px] after:rounded after:border after:border-gray-300
-										after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-[72px]
-										peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600
-										dark:bg-gray-700 dark:peer-focus:ring-blue-800"
-			></div>
-		</label>
-	)
+	const updateDish = async (id, checked) => {
+		await toggleDishState(id, checked, session)
+	}
+	return <ToggleComponent id={id} activated={activated} onUpdate={updateDish} />
 }
 
-async function toggleMenuState(id, activated, session) {
+async function toggleDishState(id, activated, session) {
 	const res = await fetch(
 		`${process.env.NEXT_PUBLIC_API_URL}/api/dishes/${id}`,
 		{
