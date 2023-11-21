3138228f64fc3b008d8dc90685c6003b856b4a48
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-21 07:01:56 +0100

    🔧 Refactor ToggleIngredientComponent and add ToggleComponent
    - Refactored ToggleIngredientComponent to use the new ToggleComponent.
    - Added a new function toggleIngredientState to handle ingredient state updates.
    - Updated the onChange event handler in the input element to call toggleIngredientState instead of toggleMenuState.
    - Created a new async function updateIngredient to handle updating the ingredient state using toggleIngredientState.
    
    This commit refactors the code for better organization and readability, and introduces a reusable ToggleComponent for handling ingredient toggling.

--- src/components/Toggle/ToggleIngredient.component.js
+++ src/components/Toggle/ToggleIngredient.component.js
@@ -3,41 +3,27 @@ import React from 'react'
 import { useSession } from 'next-auth/react'
 import { useMenusStore } from '@/stores/menu.store'
 import { getMenu } from '@/services/getMenu'
+import ToggleComponent from '@/components/Toggle/Toggle.component'
 
 export default function ToggleIngredientComponent({ id, activated, menuId }) {
 	const { data: session } = useSession()
+	// set store to update other same ingredients accross the page, not only the one we are updating
 	const setStore = useMenusStore(state => state.setMenu)
 
+	const updateIngredient = async (id, checked) => {
+		await toggleIngredientState(id, checked, session, menuId, setStore)
+	}
+
 	return (
-		<label className="relative inline-flex cursor-pointer items-center">
-			<input
-				name="activated"
-				type="checkbox"
-				id={id}
-				checked={activated}
-				onChange={e =>
-					toggleMenuState(
-						e.target.id,
-						e.target.checked,
-						session,
-						menuId,
-						setStore
-					)
-				}
-				className="peer sr-only"
-			/>
-			<div
-				className="peer h-[28px] w-[100px] rounded bg-gray-200 after:absolute after:left-[4px]
-										after:top-[4px] after:h-[20px] after:w-[20px] after:rounded after:border after:border-gray-300
-										after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-[72px]
-										peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600
-										dark:bg-gray-700 dark:peer-focus:ring-blue-800"
-			></div>
-		</label>
+		<ToggleComponent
+			id={id}
+			activated={activated}
+			onUpdate={updateIngredient}
+		/>
 	)
 }
 
-async function toggleMenuState(id, activated, session, menuId, setStore) {
+async function toggleIngredientState(id, activated, session, menuId, setStore) {
 	const res = await fetch(
 		`${process.env.NEXT_PUBLIC_API_URL}/api/ingredients/${id}`,
 		{
