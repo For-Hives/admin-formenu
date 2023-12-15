7fa2e3670a65fdc293fe53b7938645adb61658ec
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-15 22:47:38 +0100

    🔧 Refactor MenusDetails component and add new input components
    This commit refactors the MenusDetails component by adding new input components for name, description, and image of a dish. It also introduces state hooks to handle selected keys and input value changes. The Autocomplete component is updated to use the new input props and selection change handler.

--- src/components/MenusDetails.component.js
+++ src/components/MenusDetails.component.js
@@ -1,28 +1,24 @@
 'use client'
-import Image from 'next/image'
-import ToggleDishComponent from '@/components/Toggle/ToggleDish.component'
-import ToggleIngredientComponent from '@/components/Toggle/ToggleIngredient.component'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMenusStore } from '@/stores/menu.store'
 import {
 	Autocomplete,
 	AutocompleteItem,
-	Modal,
-	ModalContent,
-	ModalHeader,
-	ModalBody,
-	ModalFooter,
 	Button,
+	Chip,
+	Modal,
+	ModalBody,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
 	useDisclosure,
-	Input,
-	Link,
-	Textarea,
 } from '@nextui-org/react'
-import { customInput } from '@/styles/customConfNextui'
+import { DishDetails } from '@/components/DishDetails'
+import { InputNameDish } from '@/components/InputNameDish'
+import { InputDropzoneImageDish } from '@/components/InputDropzoneImageDish'
+import { InputDescriptionDish } from '@/components/InputDescriptionDish'
 
 /**
- * Retrieves and displays details of a menu. (main part) , and modal part ( todo : refactor & moove the modal part in a new component)
- *
  * @param {Object} menu - The menu object containing the menu details.
  * @return {JSX.Element} - The JSX element representing the menu details.
  */
@@ -32,6 +28,36 @@ export default function MenusDetails({ menu }) {
 	const setStore = useMenusStore(state => state.setMenu)
 	const setLastDishClicked = useMenusStore(state => state.setLastDishClicked)
 	const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
+	// State to store the selected keys (IDs) and input value
+	const [selectedKeys, setSelectedKeys] = useState([])
+	const [inputValue, setInputValue] = useState('')
+
+	const isIngredientSelected = ingredientId => {
+		return selectedKeys.includes(ingredientId.toString())
+	}
+
+	// Handle selection change
+	const onSelectionChange = ingredientId => {
+		setSelectedKeys(prev => {
+			if (prev.includes(ingredientId.toString())) {
+				return prev.filter(k => k !== ingredientId.toString())
+			} else {
+				return [...prev, ingredientId.toString()]
+			}
+		})
+	}
+
+	// Handle input change
+	const onInputChange = value => {
+		setInputValue(value)
+	}
+
+	useEffect(() => {
+		console.log(selectedKeys)
+		console.log(inputValue)
+	}, [selectedKeys, inputValue])
+
 	useEffect(() => {
 		if (menuFromStore.id !== menu.id) {
 			setStore(menu)
@@ -43,6 +69,26 @@ export default function MenusDetails({ menu }) {
 			console.log('lastDishClicked', lastDishClicked.ingredients)
 		}
 	}, [lastDishClicked])
+
+	const IngredientChip = ({ name, onRemove }) => (
+		<div
+			style={{
+				padding: '5px',
+				border: '1px solid gray',
+				borderRadius: '10px',
+				display: 'inline-block',
+				margin: '5px',
+			}}
+		>
+			{name}
+			<button
+				style={{ marginLeft: '10px', cursor: 'pointer' }}
+				onClick={onRemove}
+			>
+				✖
+			</button>
+		</div>
+	)
 
 	return (
 		<>
@@ -72,109 +118,9 @@ export default function MenusDetails({ menu }) {
 										>
 											{/* column 1 */}
 											<div className={'col-span-6 flex flex-col gap-3'}>
-												<div className={'flex flex-col gap-3'}>
-													<div className={'flex flex-col gap-1'}>
-														<h2>Quel est le nom de votre plat ?</h2>
-														<p>
-															Ce sera cet élément qui sera vu de prime abord et
-															qui sera afficher en premier, mettez ce que vous
-															voulez !
-														</p>
-													</div>
-													<div>
-														<Input
-															data-cy="name_dish"
-															id="name_dish"
-															name="name_dish"
-															type="text"
-															size={'sm'}
-															label="name_dish"
-															radius={'sm'}
-															variant={'bordered'}
-															color={'primary'}
-															// isInvalid={!!errors.password}
-															// errorMessage={errors.password?.message}
-															autoComplete="current-password"
-															classNames={customInput}
-														/>
-														{/*{...register('password', {*/}
-														{/*	required: true,*/}
-														{/*})}*/}
-													</div>
-												</div>
-												<div className={'flex flex-col gap-3'}>
-													<div className={'flex flex-col gap-1'}>
-														<h2>Quelle est la description de votre plat ?</h2>
-														<p>
-															Elle vous permettra de savoir à quoi correspond
-															votre plat, donner des explications
-															complémentaires, ou bien même l’histoire du plat !
-															Vous pourrez afficher toutes les informations
-															complémentaires ici.
-														</p>
-													</div>
-													<Textarea
-														data-cy="description_dish"
-														id="description_dish"
-														name="description_dish"
-														label="Description"
-														placeholder="..."
-														radius={'sm'}
-														size={'sm'}
-														variant={'bordered'}
-														className={'w-full'}
-														classNames={customInput}
-														color={'primary'}
-													/>
-												</div>
-												<div className={'flex flex-col gap-4'}>
-													<div className={'flex flex-col gap-2'}>
-														<h2>Image de présentation du plat</h2>
-														<p>
-															Vous pouvez présenter une image de votre plat,
-															elle permettra à vos clients de se trouver plus
-															simplement dans les choix qui lui sont proposés
-														</p>
-													</div>
-													<div className="flex w-full items-center justify-center rounded-md border border-cyan-900/50 bg-cyan-900/10 p-4">
-														<label
-															htmlFor="dropzone-file"
-															className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 "
-														>
-															<div className="flex flex-col items-center justify-center pb-6 pt-5">
-																<svg
-																	className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
-																	aria-hidden="true"
-																	xmlns="http://www.w3.org/2000/svg"
-																	fill="none"
-																	viewBox="0 0 20 16"
-																>
-																	<path
-																		stroke="currentColor"
-																		strokeLinecap="round"
-																		strokeLinejoin="round"
-																		strokeWidth="2"
-																		d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-																	/>
-																</svg>
-																<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-																	<span className="font-semibold">
-																		Click to upload
-																	</span>{' '}
-																	or drag and drop
-																</p>
-																<p className="text-xs text-gray-500 dark:text-gray-400">
-																	PNG, JPG, WEBP up to 1MB
-																</p>
-															</div>
-															<input
-																id="dropzone-file"
-																type="file"
-																className="hidden"
-															/>
-														</label>
-													</div>
-												</div>
+												<InputNameDish />
+												<InputDescriptionDish />
+												<InputDropzoneImageDish />
 											</div>
 											{/*fixme here*/}
 											<div className={'col-span-6 flex flex-col gap-3'}>
@@ -189,8 +135,26 @@ export default function MenusDetails({ menu }) {
 														permettrons aux clients de chercher, et retrouver
 														facilement les plats en questions.
 													</p>
+													<div className={'flex gap-2'}>
+														{selectedKeys.map(key => {
+															const ingredient =
+																lastDishClicked.ingredients.find(
+																	item => item.id.toString() === key
+																)
+															return (
+																<Chip
+																	key={key}
+																	onClose={() => onSelectionChange(key)}
+																	variant="flat"
+																>
+																	{ingredient?.name}
+																</Chip>
+															)
+														})}
+													</div>
 
 													<Autocomplete
+														inputValue={inputValue}
 														classNames={{
 															base: ['!p-0', '[&>*]:!p-0'],
 															listboxWrapper: [
@@ -201,7 +165,9 @@ export default function MenusDetails({ menu }) {
 															],
 															selectorButton: 'text-gray-700',
 														}}
-														defaultItems={lastDishClicked.ingredients}
+														items={lastDishClicked.ingredients}
+														onInputChange={onInputChange}
+														onSelectionChange={onSelectionChange}
 														inputProps={{
 															classNames: {
 																base: '!p-0',
@@ -264,22 +230,18 @@ export default function MenusDetails({ menu }) {
 														size={'sm'}
 														variant={'bordered'}
 													>
-														{item => (
-															<AutocompleteItem
-																key={item.id}
-																textValue={item.name}
-															>
-																<div className="flex items-center justify-between">
-																	<div className="flex items-center gap-2">
-																		<div className="flex flex-col">
-																			<span className="text-small">
-																				{item.name}
-																			</span>
-																		</div>
-																	</div>
-																</div>
+														{lastDishClicked.ingredients.map(item => (
+															<AutocompleteItem key={item.id}>
+																<label>
+																	<input
+																		type="checkbox"
+																		checked={isIngredientSelected(item.id)}
+																		onChange={() => onSelectionChange(item.id)}
+																	/>
+																	{item.name}
+																</label>
 															</AutocompleteItem>
-														)}
+														))}
 													</Autocomplete>
 												</div>
 											</div>
@@ -324,101 +286,5 @@ export default function MenusDetails({ menu }) {
 				</>
 			)}
 		</>
-	)
-}
-
-/**
- * Renders the details of a dish.
- * @param {object} dish - The dish object containing the dish details.
- * @param {number} menuId - The ID of the menu.
- * @param {function} onOpen - The function to be called when the link is clicked.
- * @returns {JSX.Element} The rendered dish details component.
- */
-export function DishDetails({ dish, menuId, onOpen, setLastDishClicked }) {
-	return (
-		<div
-			key={dish.id}
-			className="group relative grid w-full grid-cols-12 items-center gap-8 rounded-lg bg-white p-8 px-16 shadow-md hover:shadow-xl"
-		>
-			<Image
-				src="/icons/menu_icon_carte.svg"
-				alt={dish.name}
-				width={80}
-				height={80}
-				className="col-span-1"
-			/>
-			<div className="col-span-7 flex h-full flex-col">
-				<h2 className={'font-bold'}>
-					{' '}
-					{dish.name} - {dish.id}
-				</h2>
-				<p className={'font-light'}>{dish.description}</p>
-			</div>
-			<div className="col-span-2 flex h-full flex-col items-center justify-center">
-				<div className={'flex gap-3'}>
-					<Image
-						src="/icons/calendar_menu.svg"
-						alt="icone calendrier"
-						width={25}
-						height={25}
-					/>
-					<span className={'text-sm'}>01 Février - 24 Avril</span>
-				</div>
-			</div>
-
-			<div className="col-span-2 flex h-full items-center justify-end">
-				<ToggleDishComponent id={dish.id} activated={dish.activated} />
-			</div>
-
-			<div className="absolute right-0 top-0 z-10 p-[10px]">
-				<Link
-					type={'button'}
-					onClick={() => {
-						onOpen()
-						console.log('dish', dish)
-						setLastDishClicked(dish)
-					}}
-					className={
-						'relative flex h-[60px] w-[60px] items-center justify-center overflow-hidden'
-					}
-				>
-					<Image
-						src="/icons/change.svg"
-						alt="icone crayon"
-						width={40}
-						height={40}
-						className={
-							'absolute left-[100px] top-1/2 h-[40px] w-[40px] -translate-x-1/2 ' +
-							'-translate-y-1/2 transform transition-all hover:brightness-110 hover:saturate-150 group-hover:left-1/2'
-						}
-					/>
-				</Link>
-			</div>
-
-			<div className="col-span-11 col-start-2 flex w-full justify-end">
-				<div className={'flex w-full flex-col'}>
-					<h3 className="pb-2">Ingrédients</h3>
-					<ul className="decoration flex w-full list-disc flex-col gap-2">
-						{dish.ingredients.map(ingredient => {
-							return (
-								<li
-									key={ingredient.id}
-									className="flex w-full flex-row items-center justify-between"
-								>
-									<span>
-										{ingredient.name} - {ingredient.id}
-									</span>
-									<ToggleIngredientComponent
-										id={ingredient.id}
-										activated={ingredient.activated}
-										menuId={menuId}
-									/>
-								</li>
-							)
-						})}
-					</ul>
-				</div>
-			</div>
-		</div>
 	)
 }
