7257730ffc8ab2ea69bf665b1314e2d2d31c830f
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-09 10:48:21 +0100

    🚀 Add IngredientsModal component for managing ingredients in MenusDetails
    This commit adds a new component called IngredientsModal to the MenusDetails component. The IngredientsModal is responsible for displaying and managing the list of ingredients in a modal window. It allows users to select or deselect ingredients using checkboxes and provides buttons for closing the modal or saving changes.
    
    The commit also includes some minor changes to the InputIngredientsDish component, such as adding a button to open the IngredientsModal and commenting out an Autocomplete component temporarily.
    
    Overall, these changes enhance the user experience by providing a more intuitive way to manage ingredients in the MenusDetails component.

--- src/components/MenusDetails.component.js
+++ src/components/MenusDetails.component.js
@@ -7,6 +7,7 @@ import {
 	Button,
 	Checkbox,
 	Chip,
+	Input,
 	Modal,
 	ModalBody,
 	ModalContent,
@@ -18,15 +19,50 @@ import { DishDetails } from '@/components/DishDetails'
 import { InputNameDish } from '@/components/InputNameDish'
 import { InputDropzoneImageDish } from '@/components/InputDropzoneImageDish'
 import { InputDescriptionDish } from '@/components/InputDescriptionDish'
+import { customInput } from '@/styles/customConfNextui'
+
+function IngredientsModal({
+	isOpen,
+	onClose,
+	ingredients,
+	selectedKeys,
+	onSelectionChange,
+}) {
+	return (
+		<Modal isOpen={isOpen} onClose={onClose}>
+			<ModalHeader>Gérer les Ingrédients</ModalHeader>
+			<ModalBody>
+				<div className="grid grid-cols-3 gap-4">
+					{ingredients.map(ingredient => (
+						<div key={ingredient.id}>
+							<Checkbox
+								checked={selectedKeys.includes(ingredient.id.toString())}
+								onChange={() => onSelectionChange(ingredient.id)}
+							>
+								{ingredient.name}
+							</Checkbox>
+						</div>
+					))}
+				</div>
+			</ModalBody>
+			<ModalFooter>
+				<Button auto flat color="error" onClick={onClose}>
+					Fermer
+				</Button>
+				<Button auto onClick={onClose}>
+					Enregistrer
+				</Button>
+			</ModalFooter>
+		</Modal>
+	)
+}
 
 function InputIngredientsDish({
 	selectedKeys,
 	lastDishClicked,
 	ingredientsFromStore,
-	isIngredientSelected,
 	onSelectionChange,
-	onInputChange,
-	inputValue,
+	openIngredientsUpdate,
 }) {
 	return (
 		<div
@@ -54,93 +90,95 @@ function InputIngredientsDish({
 				})}
 			</div>
 
-			<Autocomplete
-				shouldCloseOnBlur={false}
-				inputValue={inputValue}
-				classNames={{
-					base: ['!p-0', '[&>*]:!p-0'],
-					listboxWrapper: ['!m-0', '!p-0', 'max-h-[450px]', '[&>*]:!p-0'],
-					selectorButton: 'text-gray-700',
-				}}
-				defaultItems={ingredientsFromStore}
-				onInputChange={onInputChange}
-				onSelectionChange={onSelectionChange}
-				inputProps={{
-					classNames: {
-						base: '!p-0',
-						label: 'text-gray-700',
-						input: [
-							'bg-transparent',
-							'text-gray-700/90 ',
-							'placeholder:text-gray-700/25',
-						],
-						innerWrapper: 'bg-transparent',
-						inputWrapper: [
-							'shadow-none',
-							'border',
-							'border-cyan-900/25',
-							'bg-gray-50',
-							'hover:bg-gray-100',
-							'group-data-[focused=true]:bg-gray-200',
-							'!cursor-text',
-						],
-					},
-				}}
-				listboxProps={{
-					hideSelectedIcon: true,
-					itemClasses: {
-						base: [
-							'[&>*]:!transition-none',
-							'[&>*]:!duration-0',
-							'rounded-none',
-							'text-gray-900',
-							'data-[hover=true]:text-white',
-							'data-[hover=true]:transition-none',
-							'data-[hover=true]:duration-0',
-							'data-[hover=true]:bg-sky-950',
-						],
-					},
-				}}
-				aria-label="Select an employee"
-				placeholder="Enter employee name"
-				popoverProps={{
-					triggerType: 'dialog',
-					offset: 10,
-					classNames: {
-						base: [
-							'[&>*]:!transition-none',
-							'[&>*]:!duration-0',
-							'rounded',
-							'border',
-							'!p-0',
-							'm-0',
-							'border-cyan-900/25',
-							'bg-gray-50',
-						],
-						content: 'border border-cyan-900/25 bg-gray-50 rounded !m-0 !p-0',
-					},
-				}}
-				radius={'sm'}
-				size={'sm'}
-				variant={'bordered'}
-			>
-				{/* make it depend of selected keys ( rerender the list on change of selected Keys ) */}
-				{lastDishClicked.ingredients.map(item => (
-					<AutocompleteItem key={item.id} textValue={item.name}>
-						<Checkbox
-							className={'custom-checkbox'}
-							isSelected={isIngredientSelected(item.id)}
-							onChange={() => onSelectionChange(item.id)}
-							classNames={{
-								wrapper: 'custom-icon',
-								base: 'custom-box',
-							}}
-							radius={'sm'}
-						/>
-						<label>{item.name}</label>
-					</AutocompleteItem>
-				))}
-			</Autocomplete>
+			<Button onClick={openIngredientsUpdate}>Modifier les Ingrédients</Button>
+
+			{/*<Autocomplete*/}
+			{/*	shouldCloseOnBlur={false}*/}
+			{/*	inputValue={inputValue}*/}
+			{/*	classNames={{*/}
+			{/*		base: ['!p-0', '[&>*]:!p-0'],*/}
+			{/*		listboxWrapper: ['!m-0', '!p-0', 'max-h-[450px]', '[&>*]:!p-0'],*/}
+			{/*		selectorButton: 'text-gray-700',*/}
+			{/*	}}*/}
+			{/*	defaultItems={ingredientsFromStore}*/}
+			{/*	onInputChange={onInputChange}*/}
+			{/*	onSelectionChange={onSelectionChange}*/}
+			{/*	inputProps={{*/}
+			{/*		classNames: {*/}
+			{/*			base: '!p-0',*/}
+			{/*			label: 'text-gray-700',*/}
+			{/*			input: [*/}
+			{/*				'bg-transparent',*/}
+			{/*				'text-gray-700/90 ',*/}
+			{/*				'placeholder:text-gray-700/25',*/}
+			{/*			],*/}
+			{/*			innerWrapper: 'bg-transparent',*/}
+			{/*			inputWrapper: [*/}
+			{/*				'shadow-none',*/}
+			{/*				'border',*/}
+			{/*				'border-cyan-900/25',*/}
+			{/*				'bg-gray-50',*/}
+			{/*				'hover:bg-gray-100',*/}
+			{/*				'group-data-[focused=true]:bg-gray-200',*/}
+			{/*				'!cursor-text',*/}
+			{/*			],*/}
+			{/*		},*/}
+			{/*	}}*/}
+			{/*	listboxProps={{*/}
+			{/*		hideSelectedIcon: true,*/}
+			{/*		itemClasses: {*/}
+			{/*			base: [*/}
+			{/*				'[&>*]:!transition-none',*/}
+			{/*				'[&>*]:!duration-0',*/}
+			{/*				'rounded-none',*/}
+			{/*				'text-gray-900',*/}
+			{/*				'data-[hover=true]:text-white',*/}
+			{/*				'data-[hover=true]:transition-none',*/}
+			{/*				'data-[hover=true]:duration-0',*/}
+			{/*				'data-[hover=true]:bg-sky-950',*/}
+			{/*			],*/}
+			{/*		},*/}
+			{/*	}}*/}
+			{/*	aria-label="Select an employee"*/}
+			{/*	placeholder="Enter employee name"*/}
+			{/*	popoverProps={{*/}
+			{/*		triggerType: 'dialog',*/}
+			{/*		offset: 10,*/}
+			{/*		classNames: {*/}
+			{/*			base: [*/}
+			{/*				'[&>*]:!transition-none',*/}
+			{/*				'[&>*]:!duration-0',*/}
+			{/*				'rounded',*/}
+			{/*				'border',*/}
+			{/*				'!p-0',*/}
+			{/*				'm-0',*/}
+			{/*				'border-cyan-900/25',*/}
+			{/*				'bg-gray-50',*/}
+			{/*			],*/}
+			{/*			content: 'border border-cyan-900/25 bg-gray-50 rounded !m-0 !p-0',*/}
+			{/*		},*/}
+			{/*	}}*/}
+			{/*	radius={'sm'}*/}
+			{/*	size={'sm'}*/}
+			{/*	variant={'bordered'}*/}
+			{/*>*/}
+			{/*	/!* make it depend of selected keys ( rerender the list on change of selected Keys ) *!/*/}
+			{/*	{lastDishClicked.ingredients.map(item => (*/}
+			{/*		<AutocompleteItem key={item.id} textValue={item.name}>*/}
+			{/*			<Checkbox*/}
+			{/*				className={'custom-checkbox'}*/}
+			{/*				isSelected={isIngredientSelected(item.id)}*/}
+			{/*				onChange={() => onSelectionChange(item.id)}*/}
+			{/*				classNames={{*/}
+			{/*					wrapper: 'custom-icon',*/}
+			{/*					base: 'custom-box',*/}
+			{/*				}}*/}
+			{/*				radius={'sm'}*/}
+			{/*			/>*/}
+			{/*			<label>{item.name}</label>*/}
+			{/*		</AutocompleteItem>*/}
+			{/*	))}*/}
+			{/*</Autocomplete>*/}
 		</div>
 	)
 }
@@ -161,6 +199,10 @@ export default function MenusDetails({ menu, ingredients }) {
 	// State to store the selected keys (IDs) and input value
 	const [selectedKeys, setSelectedKeys] = useState([])
 	const [inputValue, setInputValue] = useState('')
+	const [isIngredientsUpdateOpen, setIsIngredientsUpdateOpen] = useState(false)
+
+	const openIngredientsUpdate = () => setIsIngredientsUpdateOpen(true)
+	const closeIngredientsUpdate = () => setIsIngredientsUpdateOpen(false)
 
 	const isIngredientSelected = ingredientId => {
 		return selectedKeys.includes(ingredientId.toString())
@@ -228,32 +270,79 @@ export default function MenusDetails({ menu, ingredients }) {
 										<div
 											className={'grid h-full w-full grid-cols-12 gap-16 p-8'}
 										>
-											{/* column 1 */}
-											<div className={'col-span-6 flex flex-col gap-3'}>
-												<InputNameDish />
-												<InputDescriptionDish />
-												<InputDropzoneImageDish />
-											</div>
-											<div className={'col-span-6 flex flex-col gap-3'}>
-												<InputIngredientsDish
-													ingredientsFromStore={ingredientsFromStore}
-													selectedKeys={selectedKeys}
-													lastDishClicked={lastDishClicked}
-													isIngredientSelected={isIngredientSelected}
-													onSelectionChange={onSelectionChange}
-													onInputChange={onInputChange}
-													inputValue={inputValue}
-												/>
-											</div>
+											{!isIngredientsUpdateOpen ? (
+												<>
+													<div className={'col-span-6 flex flex-col gap-3'}>
+														<InputNameDish />
+														<InputDescriptionDish />
+														<InputDropzoneImageDish />
+													</div>
+													<div className={'col-span-6 flex flex-col gap-3'}>
+														<InputIngredientsDish
+															ingredientsFromStore={ingredientsFromStore}
+															selectedKeys={selectedKeys}
+															lastDishClicked={lastDishClicked}
+															isIngredientSelected={isIngredientSelected}
+															onSelectionChange={onSelectionChange}
+															onInputChange={onInputChange}
+															inputValue={inputValue}
+															openIngredientsUpdate={openIngredientsUpdate}
+															closeIngredientsUpdate={closeIngredientsUpdate}
+														/>
+													</div>
+												</>
+											) : (
+												<>
+													<div className={'col-span-12 flex flex-col gap-3'}>
+														<div className={'grid grid-cols-3 gap-4'}>
+															{ingredientsFromStore.map(ingredient => (
+																<div key={ingredient.id}>
+																	<Checkbox
+																		checked={selectedKeys.includes(
+																			ingredient.id.toString()
+																		)}
+																		onChange={() =>
+																			onSelectionChange(ingredient.id)
+																		}
+																	>
+																		{ingredient.name}
+																	</Checkbox>
+																</div>
+															))}
+														</div>
+													</div>
+												</>
+											)}
 										</div>
 									</ModalBody>
 									<ModalFooter>
-										<Button color="danger" variant="flat" onPress={onClose}>
-											Close
-										</Button>
-										<Button color="primary" onPress={onClose}>
-											Sign in
-										</Button>
+										{!isIngredientsUpdateOpen ? (
+											<>
+												{/*todo on close event*/}
+												<Button color="danger" variant="flat" onPress={onClose}>
+													Fermer
+												</Button>
+												<Button color="primary" onPress={onClose}>
+													Enregistrer
+												</Button>
+											</>
+										) : (
+											<>
+												<Button
+													color="danger"
+													variant="flat"
+													onPress={closeIngredientsUpdate}
+												>
+													Revenir en arrière
+												</Button>
+												<Button
+													color="primary"
+													onPress={closeIngredientsUpdate}
+												>
+													Enregistrer les ingrédients
+												</Button>
+											</>
+										)}
 									</ModalFooter>
 								</>
 							)}
