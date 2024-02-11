2e0fc5cdc68a3b95e23b8b44679af2d026ef26ff
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-11 17:32:03 +0100

    🔧 Add toggleAllergensState function
    This commit adds a new function called toggleAllergensState to the switchElementsActivation.js file. This function allows for toggling the state of allergens for a dish. It makes a PUT request to the API with the updated data and displays success or error messages using toast notifications.

--- src/services/switchElementsActivation.js
+++ src/services/switchElementsActivation.js
@@ -36,6 +36,46 @@ export async function toggleDishState(id, activated, session) {
 	return res.json()
 }
 
+export async function toggleAllergensState(
+	id,
+	name_property,
+	activated,
+	session
+) {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/dishes/${id}`,
+		{
+			method: 'PUT',
+			headers: {
+				// 	token
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				Authorization: `Bearer ${session.jwt}`,
+			},
+			body: JSON.stringify({
+				data: {
+					[name_property]: activated,
+				},
+			}),
+		}
+	)
+
+	if (!res.ok) {
+		toast('Une erreur est survenue, veuillez réessayer plus tard', {
+			type: 'error',
+			icon: '⛔',
+			toastId: 'toast-alert',
+		})
+	} else {
+		toast('Allergène modifié avec succès', {
+			type: 'success',
+			icon: '👌',
+			toastId: 'toast-alert',
+		})
+	}
+	return res.json()
+}
+
 export async function toggleIngredientState(
 	id,
 	activated,
