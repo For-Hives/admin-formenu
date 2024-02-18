d4ac9ca2482a3a3af28d060ed6896793c8b80df4
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-18 18:23:06 +0100

    ✨ Update input component with Select for category selection
    Added a Select component to allow users to choose a category for their dish, displaying categories dynamically. Also improved error handling and styling.

--- src/components/InputCategoryDish.component.js
+++ src/components/InputCategoryDish.component.js
@@ -1,44 +1,60 @@
 import { Controller } from 'react-hook-form'
-import { Input } from '@nextui-org/react'
+import { Input, Select, SelectItem } from '@nextui-org/react'
 import { customInput } from '@/styles/customConfNextui'
 
-export function InputCategoryDishComponent({ control, errors, name, value }) {
+export function InputCategoryDishComponent({
+	control,
+	errors,
+	name,
+	value,
+	category,
+	categories,
+}) {
+	console.log('value', value)
 	return (
 		<div className={'flex flex-col gap-3'}>
 			<div className={'flex flex-col gap-1'}>
 				<h2 className={'font-kanit text-lg font-medium'}>
-					Quel est la catégorie de votre plat ? ( entrée / plat / dessert ... ?
-					)
+					Quel est la catégorie de votre plat ? (&nbsp;entrée / plat / dessert
+					... ?&nbsp;)
 				</h2>
 				<p className={'text-sm italic'}>
 					Ici vous pouvez choisir la catégorie de votre plat. Vous la
 					retrouverez dans la carte directement ici !
 				</p>
 			</div>
-			{/*<div>*/}
-			{/*	<Controller*/}
-			{/*		name={name ?? ''}*/}
-			{/*		control={control}*/}
-			{/*		defaultValue={value ?? ''} // Use Controller's defaultValue to set initial value*/}
-			{/*		render={({ field }) => (*/}
-			{/*			<Input*/}
-			{/*				{...field}*/}
-			{/*				data-cy={name}*/}
-			{/*				id={name}*/}
-			{/*				name={name}*/}
-			{/*				type="number"*/}
-			{/*				size="sm"*/}
-			{/*				placeholder="Prix du plat..."*/}
-			{/*				radius="sm"*/}
-			{/*				variant="bordered"*/}
-			{/*				color="primary"*/}
-			{/*				classNames={customInput}*/}
-			{/*				isInvalid={!!errors[name]}*/}
-			{/*				errorMessage={errors[name]?.message}*/}
-			{/*			/>*/}
-			{/*		)}*/}
-			{/*	/>*/}
-			{/*</div>*/}
+			<div>
+				<Controller
+					name={name ?? ''}
+					control={control}
+					defaultValue={value ?? ''}
+					render={({ field }) => (
+						<Select
+							{...field}
+							placeholder="Catégorie du plat..."
+							classNames={customInput}
+							color="primary"
+							variant="bordered"
+							radius="sm"
+							selectedKeys={value.toString()}
+							data-cy={name}
+							id={name}
+							name={name}
+							size="sm"
+							isInvalid={!!errors[name]}
+							errorMessage={errors[name]?.message}
+						>
+							{categories?.length > 0 &&
+								categories.map(category => (
+									<SelectItem key={category.id} value={category.id}>
+										{category.attributes.name}
+									</SelectItem>
+								))}
+						</Select>
+					)}
+				/>
+				<span>{value.id}</span>
+			</div>
 		</div>
 	)
 }
