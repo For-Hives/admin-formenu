faae7fa96c3703e98be908698769aefae641c4a1
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-25 17:36:40 +0100

    ✨ Add imports and set up Home function with authentication check
    Imported necessary modules for authentication handling and added logic to check session status before displaying ingredients table in the Home component.

---
+++ src/app/ingredients/page.js
@@ -0,0 +1,25 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import Nav from '@/components/Nav/Nav.component'
+import { redirect } from 'next/navigation'
+import { IngredientsTable } from '@/app/ingredients/ingredientsTable'
+import { getIngredients } from '@/services/getIngredients'
+
+export default async function Home() {
+	const session = await getServerSession(authOptions)
+	if (!session) {
+		redirect('/auth/signin')
+	}
+
+	const ingredients = await getIngredients(session)
+
+	return (
+		<>
+			<Nav />
+			{/* +4rem -> equivalent of pr-16 / pl-16, space to don't be under the nav bar  */}
+			<main className="flex min-h-screen w-full items-center justify-center overflow-hidden py-8 pl-[calc(250px+4rem)] pr-16">
+				<IngredientsTable ingredients={ingredients} />
+			</main>
+		</>
+	)
+}
