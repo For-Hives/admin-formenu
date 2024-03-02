f3f38f82af66a52c7d2b420ac2012d636d8d3097
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-02 19:52:13 +0100

    ✨ Add new services and data to DishTableComponent
    Expanded the Page component to fetch ingredients, type dishes, categories filtered by depth, allergens list, and diets list. Updated DishTableComponent props accordingly.

--- src/app/plats/page.js
+++ src/app/plats/page.js
@@ -4,6 +4,11 @@ import { redirect } from 'next/navigation'
 import Nav from '@/components/Nav/Nav.component'
 import { getDishes } from '@/services/dish/getDishes'
 import { DishTableComponent } from '@/components/Dish/Table/DishTable.component'
+import { getIngredients } from '@/services/ingredients/getIngredients'
+import { getTypeDishes } from '@/services/type_dishes/getTypeDishes'
+import { getCategoriesFilteredByDepth } from '@/services/categories/getCategoriesFilteredByDepth'
+import { allergensList } from '@/services/allergens/getAllergens'
+import { dietsList } from '@/services/diets/getDiets'
 
 export default async function Page() {
 	const session = await getServerSession(authOptions)
@@ -12,12 +17,23 @@ export default async function Page() {
 	}
 
 	const dishes = await getDishes(session)
+	const ingredients = await getIngredients(session)
+	const typeDishes = await getTypeDishes(session)
+	const categories = await getCategoriesFilteredByDepth(session, 1)
 
 	return (
 		<>
 			<Nav />
 			<main className="flex min-h-screen w-full items-center justify-center overflow-hidden py-8 pl-[calc(250px+4rem)] pr-16">
-				<DishTableComponent dishBase={dishes} session={session} />
+				<DishTableComponent
+					dishBase={dishes}
+					ingredients={ingredients}
+					allergens={allergensList}
+					diets={dietsList}
+					session={session}
+					categories={categories?.data}
+					typeDishes={typeDishes?.data}
+				/>
 			</main>
 		</>
 	)
