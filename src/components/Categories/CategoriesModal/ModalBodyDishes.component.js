5a4fe3da4eb67cacf959860c7b9b28449a5b1f39
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-03 21:36:26 +0100

    ✨ Add new input and checkbox components
    Added new components for input fields and checkboxes in different categories modal. The InputNameCategoryComponent handles category name input, while the ModalBodyIngredientsComponent manages ingredient selection checkboxes. Also, refactored and renamed ModalFooterBack component.

---
+++ src/components/Categories/CategoriesModal/ModalBodyDishes.component.js
@@ -0,0 +1,24 @@
+import { Checkbox } from '@nextui-org/react'
+
+export function ModalBodyIngredientsComponent({
+	ingredientsFromStore,
+	isIngredientSelected,
+	onSelectionChange,
+}) {
+	return (
+		<div className={'col-span-12 flex flex-col gap-3'}>
+			<div className={'grid grid-cols-3 gap-4'}>
+				{ingredientsFromStore.map(ingredient => (
+					<div key={ingredient.id}>
+						<Checkbox
+							isSelected={isIngredientSelected(ingredient.id)}
+							onChange={() => onSelectionChange(ingredient.id)}
+						>
+							{ingredient.name}
+						</Checkbox>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
