8aca2ad3aa7a8bdcc854b89a0158be959d63e274
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-03 20:26:16 +0100

    ✨ Add columns to categories data
    Added new columns for ID, NAME, ORDER, DEPTH, DISHES, and ACTIONS in the categories data.

---
+++ src/components/Categories/data.js
@@ -0,0 +1,8 @@
+export const columnsCategories = [
+	{ name: 'ID', uid: 'id', sortable: true },
+	{ name: 'NAME', uid: 'name', sortable: true },
+	{ name: 'ORDER  ', uid: 'order', sortable: true },
+	{ name: 'DEPTH', uid: 'depth', sortable: true },
+	{ name: 'DISHES', uid: 'dishes', sortable: true },
+	{ name: 'ACTIONS', uid: 'actions' },
+]
