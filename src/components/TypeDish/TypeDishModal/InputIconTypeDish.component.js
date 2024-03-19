c6e4ff06f34513847ed6b96d2b3c2d15cd5590eb
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-19 15:40:57 +0100

    ✨ Add image upload functionality to TypeDish modal component
    Implement the ability to upload and display an icon for representing a type of dish in the TypeDish modal component. Includes handling file selection, uploading, and displaying the uploaded image.

---
+++ src/components/TypeDish/TypeDishModal/InputIconTypeDish.component.js
@@ -0,0 +1,93 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+import { uploadFile } from '@/services/uploadFile'
+
+export function InputIconTypeDishComponent({
+	control,
+	errors,
+	name,
+	value,
+	session,
+}) {
+	const [uploading, setUploading] = useState(false)
+	const [uploadedImage, setUploadedImage] = useState(value)
+
+	const handleFileChange = async event => {
+		const file = event.target.files[0]
+		if (!file) return
+
+		try {
+			const { data, res } = await uploadFile(session, file)
+			if (res.ok) {
+				// Assuming the first file in the array is the one we're interested in
+				const uploadedFile = data[0]
+				setUploadedImage(uploadedFile) // Update the state with the uploaded image
+			}
+		} catch (e) {
+			console.error(e)
+		} finally {
+			setUploading(false)
+		}
+	}
+
+	return (
+		<div className={'flex flex-col gap-4'}>
+			<div className={'flex flex-col gap-2'}>
+				<h2 className={'font-kanit text-lg font-medium'}>
+					Icône du type de plat
+				</h2>
+				<p className={'text-sm italic'}>
+					Vous pouvez ajouter une icône pour représenter ce type de plat.
+				</p>
+			</div>
+			<div className="group relative flex w-full items-center justify-center rounded-md border border-cyan-900/50 bg-cyan-900/10 p-4">
+				{uploadedImage?.url && (
+					<Image
+						src={uploadedImage.url}
+						fill={true}
+						alt={uploadedImage.alternativeText ?? 'icône'}
+						className={
+							'pointer-events-none z-10 object-cover opacity-100 transition group-hover:opacity-0'
+						}
+					/>
+				)}
+				<label
+					htmlFor="dropzone-file"
+					className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100"
+				>
+					<div className="flex flex-col items-center justify-center pb-6 pt-5">
+						<svg
+							className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 20 16"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+							/>
+						</svg>
+						<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+							<span className="font-semibold">Cliquez</span> ou glissez-déposez
+							une image ici.
+						</p>
+						<p className="text-xs text-gray-500 dark:text-gray-400">
+							SVG jusqu'à 1MB
+						</p>
+					</div>
+					<input
+						id="dropzone-file"
+						type="file"
+						className="hidden"
+						onChange={handleFileChange}
+					/>
+				</label>
+			</div>
+		</div>
+	)
+}
