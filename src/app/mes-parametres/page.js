6c3e9812e9f1c192e088cd663fe38c82309d9bad
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-19 16:21:38 +0100

    ✨ Add typeDishes data to TypeDishTableComponent props
    Updated TypeDishTableComponent props to include typeDishes data for display.

--- src/app/mes-parametres/page.js
+++ src/app/mes-parametres/page.js
@@ -12,13 +12,17 @@ export default async function Home() {
 	}
 
 	const typeDishes = await getTypeDishes(session)
+	console.log(typeDishes.data[0].attributes)
 
 	return (
 		<>
 			<Nav />
 			{/* +4rem -> equivalent of pr-16 / pl-16, space to don't be under the nav bar  */}
 			<main className="flex min-h-screen w-full items-center justify-center overflow-hidden py-8 pl-[calc(250px+4rem)] pr-16">
-				<TypeDishTableComponent typeDishesBase={typeDishes} session={session} />
+				<TypeDishTableComponent
+					typeDishesBase={typeDishes.data}
+					session={session}
+				/>
 			</main>
 		</>
 	)
