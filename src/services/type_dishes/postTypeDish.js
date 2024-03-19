3caa4d3565f596aaf3dfbad2a6ccd2cfa6a0c5ea
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-19 15:43:40 +0100

    ✨ Add toast notifications for postTypeDish function
    Imported react-toastify to display success/error messages when adding a new dish type.Implemented logic to show appropriate toast messages based on the API response status.

---
+++ src/services/type_dishes/postTypeDish.js
@@ -0,0 +1,34 @@
+import { toast } from 'react-toastify'
+
+export async function postTypeDish(typeDish, session) {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/type-dishes`,
+		{
+			method: 'POST',
+			headers: {
+				// 	token
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				Authorization: `Bearer ${session.jwt}`,
+			},
+			body: JSON.stringify({
+				data: typeDish,
+			}),
+		}
+	)
+
+	if (!res.ok) {
+		toast('Une erreur est survenue, veuillez réessayer plus tard', {
+			type: 'error',
+			icon: '⛔',
+			toastId: 'toast-alert',
+		})
+	} else {
+		toast('Type de plat ajouté avec succès', {
+			type: 'success',
+			icon: '👌',
+			toastId: 'toast-alert',
+		})
+	}
+	return res.json()
+}
