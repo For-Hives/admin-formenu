117388366f7e64f21cbbfe61b540034e7de8e170
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-19 12:19:16 +0100

    ✨ Refactor signout logic and useEffect in page component
    Simplified the signout process by updating the useEffect hook to directly call signOut with callbackUrl and redirect options. Removed unnecessary session and router dependencies.

--- src/app/auth/signout/page.js
+++ src/app/auth/signout/page.js
@@ -1,25 +1,13 @@
+// auth/signout/page.js
 import { LoaderComponent } from '@/components/Loaders/Loader.component'
 
 import { useEffect } from 'react'
-import { signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { signOut } from 'next-auth/react'
 
 function Signout() {
-	const { data: session, status } = useSession()
-	const router = useRouter()
-
 	useEffect(() => {
-		// If the user is logged in, proceed to sign them out
-		if (status === 'authenticated') {
-			signOut({ redirect: false }).then(() => {
-				// Redirect the user to the signin page after signing out
-				router.push('/auth/signin')
-			})
-		} else {
-			// If the user is not logged in, redirect immediately
-			router.push('/auth/signin')
-		}
-	}, [session, router, status])
+		signOut({ callbackUrl: '/auth/signin', redirect: true })
+	}, [])
 
 	// Display a loader while the sign-out process is in progress
 	return <LoaderComponent />
