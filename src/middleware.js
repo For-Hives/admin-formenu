1bcf406bcf36373b367b3dbd66564ca64570e3d9
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-19 12:19:32 +0100

    ✨ Refactor middleware logic for session handling
    Remove unnecessary console log and expired session check. Simplify the middleware function to handle sessions effectively.

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,4 @@
+// /middleware.js
 export { default } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
@@ -7,19 +8,11 @@ const secret = process.env.NEXTAUTH_SECRET
 export async function middleware(req) {
 	const session = await getToken({ req, secret })
 
-	console.log('session', session)
-
 	if (!session) {
 		const url = req.nextUrl.clone()
 		url.pathname = '/auth/signin'
 		return NextResponse.redirect(url)
 	}
 
-	// convert exp (in timestamp) to date now and compare it to the current date
-	if (session.exp * 1000 > Date.now()) {
-		const url = req.nextUrl.clone()
-		url.pathname = '/auth/signin'
-		return NextResponse.redirect(url)
-	}
 	return NextResponse.next()
 }
