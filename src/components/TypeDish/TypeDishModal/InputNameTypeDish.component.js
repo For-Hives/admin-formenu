8e44c56d3fc8bf903e0a03f48d200748f28a17be
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-19 15:39:41 +0100

    ✨ Add InputNameTypeDishComponent for type of dish name
    Created a new component to handle input for the type of dish name in a modal. Used Next.js Input component and React Hook Form Controller for form management.

---
+++ src/components/TypeDish/TypeDishModal/InputNameTypeDish.component.js
@@ -0,0 +1,38 @@
+import { Input } from '@nextui-org/react'
+import { customInput } from '@/styles/customConfNextui'
+import { Controller } from 'react-hook-form'
+
+export function InputNameTypeDishComponent({ control, errors, name, value }) {
+	return (
+		<div className={'flex flex-col gap-3'}>
+			<div className={'flex flex-col gap-1'}>
+				<h2 className={'font-kanit text-lg font-medium'}>
+					Quel est le nom du type de plat ?
+				</h2>
+			</div>
+			<div>
+				<Controller
+					name={name ?? ''}
+					control={control}
+					defaultValue={value ?? ''} // Use Controller's defaultValue for initial form value
+					render={({ field }) => (
+						<Input
+							{...field}
+							data-cy={name}
+							id={name}
+							type="text"
+							size="sm"
+							placeholder="Nom du type de plat..."
+							radius="sm"
+							variant="bordered"
+							color="primary"
+							isInvalid={!!errors[name]}
+							errorMessage={errors[name]?.message}
+							classNames={customInput}
+						/>
+					)}
+				/>
+			</div>
+		</div>
+	)
+}
